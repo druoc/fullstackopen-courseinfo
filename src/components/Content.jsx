78eb262315cf6206543753dc89bdcfe95f4995f9
Dave <d.ruocco1987@googlemail.com>
2023-07-20 12:51:37 +0100

    Refactors components

---
+++ src/components/Content.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import Part from "./Part";
+const Content = ({
+  part1,
+  part2,
+  part3,
+  exercises1,
+  exercises2,
+  exercises3,
+}) => {
+  return (
+    <div>
+      <Part part={part1} exercise={exercises1} />
+      <Part part={part2} exercise={exercises2} />
+      <Part part={part3} exercise={exercises3} />
+    </div>
+  );
+};
+
+export default Content;
---
+++ src/components/Part.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+const Part = ({ part, exercise }) => {
+  return (
+    <div>
+      <p>
+        {part} {exercise}
+      </p>
+    </div>
+  );
+};
+
+export default Part;
